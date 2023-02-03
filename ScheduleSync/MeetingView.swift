41427daa79600e44221baadc98843e1baadaa1ab
Snarr <jacobsnarr@gmail.com>
2023-02-03 11:09:56 -0500

    Initialize project and follow tutorial part 1
    - Following Apple "Scrumdinger" tutorial
    - Finish first part "using stacks to arrange views"

---
+++ ScheduleSync/MeetingView.swift
@@ -0,0 +1,49 @@
+//
+//  ContentView.swift
+//  ScheduleSync
+//
+//  Created by Jacob Snarr on 2/3/23.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                        .font(.caption)
+                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining").font(.caption)
+                    Label("600", systemImage: "hourglass.tophalf.fill")
+                }
+            }
+            .accessibilityElement(children: .ignore) // Ignore the inferred accessibility labels and values for child views (prevents voiceover from reading names of icons)
+            .accessibilityLabel("Time remaining")
+            .accessibilityValue("10 minutes")
+            Circle()
+                .strokeBorder(lineWidth: 24)
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}) {
+                    Image(systemName: "forward.fill")
+                }
+                .accessibilityLabel("Next speaker")
+                .accessibilityValue("1 of 3")
+            }
+        }
+        .padding()
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
--- ScheduleSync/ScheduleSyncApp.swift
+++ ScheduleSync/ScheduleSyncApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ScheduleSyncApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MeetingView()
         }
     }
 }
