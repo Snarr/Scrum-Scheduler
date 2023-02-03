335f752b2d9aed78386b090583a70f81b2320f67
Snarr <jacobsnarr@gmail.com>
2023-02-03 10:45:07 -0500

    Initial Commit

---
+++ ScheduleSync/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ScheduleSync
+//
+//  Created by Jacob Snarr on 2/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ScheduleSync/ScheduleSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScheduleSyncApp.swift
+//  ScheduleSync
+//
+//  Created by Jacob Snarr on 2/3/23.
+//
+
+import SwiftUI
+
+@main
+struct ScheduleSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
